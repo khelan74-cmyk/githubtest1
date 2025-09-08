5179eb59a2a4bb25d5e603c45220fb659eb8303b
khelan74-cmyk <khelan.74@gmail.com>
2025-09-08 11:45:05 +0200

    created a pythonfile

---
+++ code_01_03.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+'''
+driver = webdriver.Firefox()
+driver.get("http://www.python.org")
+elem = driver.find_element(By.NAME, "q")
+
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+time.sleep(8)
+
+driver.close()
+'''
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
+driver.close()
